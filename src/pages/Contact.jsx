9a9ab8ba0b52c963ba6fbf4027609a9a7aad5a7d
Jorge Leonardo Hernandez Rozo <seergee18@gmail.com>
2024-07-22 20:56:18 -0500

    adding card example

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,36 @@
+const emojis = [
+    {
+        emoji: "😀",
+        description: "Cara sonriente"
+    },
+    {
+        emoji: "😂",
+        description: "Cara con lágrimas de risa"
+    },
+    {
+        emoji: "😍",
+        description: "Cara con ojos de corazón"
+    },
+    {
+        emoji: "🤔",
+        description: "Cara pensando"
+    }
+]
 export const Contact = () => {
     return (
         <div className="contactPage pageContainer">
             <h1>Contact View</h1>
+            <p className="contactParagraph">Reading a array of objects</p>
+            <div className="contactCardContainer">
+                {emojis.map((emoji, index) => (
+                    <div key={index} className="contactCard">
+                        <span >
+                            {emoji.emoji}
+                            <p>{emoji.description}</p>
+                        </span>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
